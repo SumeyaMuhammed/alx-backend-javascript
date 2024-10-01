71356993df99ee27d5a85518099ce0344d186918
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-30 14:30:57 -1100

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,12 @@
-export default function createIteratorObject(report) {}
+const engineering = createEmployeesObject("engineering", [
+  "John Doe",
+  "Guillaume Salva",
+]);
+const sales = createEmployeesObject("sales", ["Alice", "Bob"]);
+const report = createReportObject({ ...engineering, ...sales });
+
+const employeeIterator = createIteratorObject(report);
+
+for (const employee of employeeIterator) {
+  console.log(employee);
+}
