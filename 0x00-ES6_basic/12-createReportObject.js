e5bca145e3757c559ffe19a51621d95203542054
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-30 14:28:11 -1100

    create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,10 @@
-export default function createReportObject(employeesList) {}
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+}
