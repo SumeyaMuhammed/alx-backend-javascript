ce93d0bbb8bba913d50c242286492fc3d498dae0
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-30 14:32:34 -1100

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,11 @@
-export default function iterateThroughObject(reportWithIterator) {}
+const engineering = createEmployeesObject("engineering", [
+  "John Doe",
+  "Guillaume Salva",
+]);
+const sales = createEmployeesObject("sales", ["Alice", "Bob"]);
+const report = createReportObject({ ...engineering, ...sales });
+
+const reportWithIterator = createIteratorObject(report);
+const employeeList = iterateThroughObject(reportWithIterator);
+
+console.log(employeeList);
