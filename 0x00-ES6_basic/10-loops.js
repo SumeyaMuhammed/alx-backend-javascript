3bf93e3a526a381b084914cd80e958a5e47f4a3a
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-10-02 11:21:30 -1100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
+  for (const value of array) {
     const index = array.indexOf(value);
     array[index] = appendString + value;
   }
