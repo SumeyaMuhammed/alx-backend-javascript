0dfbd493e3d2d1746feddcc25f8202aa33e392f4
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-02 09:16:22 -1100

    export

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,11 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve("Response from API");
-      } else {
-        reject(new Error("something went wrong"));
-      }
-    }, 1000);
+    const success = true;
+
+    if (success) {
+      resolve("Response from API");
+    } else {
+      reject(new Error("Error: Unable to get response from API"));
+    }
   });
 }
