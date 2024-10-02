37bf4dfdd15585b629184e8034aea784a5aa074e
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-02 09:18:22 -1100

    export

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,21 +1,12 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success',
+        body: "Success",
       });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
-
-// Example usage
-getFullResponseFromAPI(true)
-  .then((response) => {
-    console.log(response); // Output: { status: 200, body: 'Success' }
-  })
-  .catch((error) => {
-    console.error(error); // Output: Error: The fake API is not working currently
-  });
