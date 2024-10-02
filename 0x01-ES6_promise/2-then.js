81ce838a5ebed81539c31555e46b0967ed786fa4
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-10-02 11:56:14 -1100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log("Got a response from the API");
+      console.log('Got a response from the API');
       return {
         status: 200,
-        body: "success",
+        body: 'success',
       };
     })
     .catch(() => {
