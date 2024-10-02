4f731c457dff8b21f416996148f8d3af5f990e71
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-02 09:28:31 -1100

    export n import

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+
+      console.log(
+        photoResponse.body,
+        userResponse.firstName,
+        userResponse.lastName
+      );
+    })
+    .catch(() => {
+      console.log("Signup system offline");
+    });
+}
