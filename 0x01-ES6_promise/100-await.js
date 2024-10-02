a6c182d45db0c422d4e9989db20091aec9326a7f
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-10-02 12:07:12 -1100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
-import uploadPhoto from "./5-photo-reject";
-import createUser from "./4-user-promise";
+import uploadPhoto from './5-photo-reject';
+import createUser from './4-user-promise';
 
 export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto("photo.jpg");
-    const user = await createUser("Guillaume", "Salva");
+    const photo = await uploadPhoto('photo.jpg');
+    const user = await createUser('Guillaume', 'Salva');
 
     return {
-      photo: photo,
-      user: user,
+      photo,
+      user,
     };
   } catch (error) {
     return {
