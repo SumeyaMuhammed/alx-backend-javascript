0a9798784e586d7b8541e6b87e8f5406e3eeff3e
Sumeya Muhammed <159241443+SumeyaMuhammed@users.noreply.github.com>
2024-10-02 11:54:36 -1100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
@@ -12,6 +12,6 @@ export default function handleProfileSignup() {
       );
     })
     .catch(() => {
-      console.log("Signup system offline");
+      console.log('Signup system offline');
     });
 }
