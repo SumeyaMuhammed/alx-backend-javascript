3de26751cfccd0f737d918811ae3b1c492c40b2f
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-03 21:40:29 -1100

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,12 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled(promises).then(results => {
-    return results.map(result => {
-      if (result.status === 'fulfilled') {
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((result) => {
+      if (result.status === "fulfilled") {
         return {
           status: result.status,
           value: result.value,
