57d67a9a4aa38b132898558b218912f5a6354c0f
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-04 03:31:29 -1100

    hbtn

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,17 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    if (typeof length !== "number") {
+      throw new TypeError("Length must be a number");
+    }
+    if (
+      !Array.isArray(students) ||
+      !students.every((student) => typeof student === "string")
+    ) {
+      throw new TypeError("Students must be an array of strings");
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -32,8 +44,11 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!(students instanceof Array)) {
-      throw new TypeError("Students must be an array");
+    if (
+      !Array.isArray(students) ||
+      !students.every((student) => typeof student === "string")
+    ) {
+      throw new TypeError("Students must be an array of strings");
     }
     this._students = students;
   }
