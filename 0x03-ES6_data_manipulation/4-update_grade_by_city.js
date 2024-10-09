8a1b1ea2761a62a3aac378cba3ed82ece7221a3b
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-08 14:47:47 -1100

    update grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : "N/A",
+      };
+    });
+}
