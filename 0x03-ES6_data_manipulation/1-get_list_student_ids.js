ac80e675efa7230f3c662fa25df4f5746794fa6d
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-08 13:59:22 -1100

    remove else

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(arr) {
-  if (arr instanceof Array) {
-    return arr.map((arr) => arr.id);
-  } else {
+  if (!(arr instanceof Array)) {
     return [];
   }
+  return arr.map((arr) => arr.id);
 }
