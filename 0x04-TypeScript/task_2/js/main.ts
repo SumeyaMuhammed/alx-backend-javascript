39e4aac6d795ce4d1ef250c3f30843bb0faa3be0
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-09 13:52:50 -1100

    task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,46 @@
+interface DirectorInterface {
+  workFromHome(): String;
+  getCoffeeBreak(): String;
+  workDirectorTasks(): String;
+}
+
+interface TeacherInterface {
+  workFromHome(): String;
+  getCoffeeBreak(): String;
+  workTeacherTasks(): String;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
