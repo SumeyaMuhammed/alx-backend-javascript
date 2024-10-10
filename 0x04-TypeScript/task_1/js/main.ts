2ebcace5b77258a2c2bf3672a1555ee4ed398b17
Sumeya Muhammed <sumenaweya@gmail.com>
2024-10-09 13:31:30 -1100

    director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,10 +7,6 @@ interface Teacher {
   [key: string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: "John",
-  fullTimeEmployee: false,
-  lastName: "Doe",
-  location: "London",
-  contract: false,
-};
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
